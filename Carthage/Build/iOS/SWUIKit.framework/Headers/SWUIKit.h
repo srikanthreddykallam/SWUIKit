fef0713fe3893e0262f72fd147724b32120ef6d7
Srikanth Reddy Kallam <skallam2@sapient.com>
2019-10-09 17:16:12 +0100

    Added carthage support

---
+++ Carthage/Build/iOS/SWUIKit.framework/Headers/SWUIKit.h
@@ -0,0 +1,19 @@
+//
+//  SWUIKit.h
+//  SWUIKit
+//
+//  Created by Srikanth Reddy Kallam on 09/10/19.
+//  Copyright © 2019 Srikanth Reddy Kallam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SWUIKit.
+FOUNDATION_EXPORT double SWUIKitVersionNumber;
+
+//! Project version string for SWUIKit.
+FOUNDATION_EXPORT const unsigned char SWUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWUIKit/PublicHeader.h>
+
+
