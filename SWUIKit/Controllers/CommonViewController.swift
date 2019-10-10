cab0f2e15ff71eeb111be231eac3ea7884dec1e8
Srikanth Reddy Kallam <skallam2@sapient.com>
2019-10-10 13:55:24 +0100

    Added RxSwift dependency on framework.

--- SWUIKit/Controllers/CommonViewController.swift
+++ SWUIKit/Controllers/CommonViewController.swift
@@ -1,7 +1,11 @@
 import UIKit
+import RxSwift
+
 
 public class CommonViewController: UIViewController {
+    public let timeRx = RxTime()
     override public func viewDidLoad() {
         super.viewDidLoad()
+        print(rx)
     }
 }
